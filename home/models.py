df6393481780d0d3e90721069b807aef5d47e405
DanielMaiaDeSousa <daniel.maia.sousa@gmail.com>
2026-01-11 20:49:42 +0000

    Configuração de ALLOWED_HOSTS e finalização do CRUD

--- home/models.py
+++ home/models.py
@@ -9,5 +9,29 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.nome
+    
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=14)
+    datanasc = models.DateField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome
+
+    @property
+    def datanascimento(self):
+        if self.datanasc:
+            return self.datanasc.strftime('%d/%m/%Y')
+        return None
+    
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    img_base64 = models.TextField(blank=True) # Para armazenar imagem em base64
+
+    def __str__(self):
+        return self.nome    
 
 
